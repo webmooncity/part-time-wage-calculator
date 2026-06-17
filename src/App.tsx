380a13b34c47ccc05bcca1dc81d821305d774acc
WebMoonCity <webmooncity@gmail.com>
2026-06-18 07:00:06 +0900

    Initial React TypeScript setup.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <h1>단기 알바 시급-일급 계산 웹 앱</h1>
+      <p>근무시간과 시급을 입력하면 일급을, 그리고 근무시간과 일급을 입력하면 시급을 계산합니다.</p>
+    </main>
+  );
+
+}
+export default App
